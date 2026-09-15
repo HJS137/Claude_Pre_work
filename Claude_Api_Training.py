9aea6b705a4c11fd73a4ca7f56bd1077bbdbf0b4
HJS137 <harry.stevens@talktalk.net>
2026-09-15 10:12:23 +0100

    Lesson 4: Multi turn conversation

--- Claude_Api_Training.py
+++ Claude_Api_Training.py
@@ -22,3 +22,43 @@ message = client.messages.create(
 )
 
 print(message.content[0].text)
+
+
+### Multi- Turn Conversations 
+### Building helper functions
+
+def add_user_message(messages, text):
+    user_message = {"role": "user", "content": text}
+    messages.append(user_message)
+
+def add_assistant_message(messages, text):
+    assistant_message = {"role": "assistant", "content": text}
+    messages.append(assistant_message)
+
+def chat(messages):
+    message = client.messages.create(
+        model=model,
+        max_tokens=1000,
+        messages=messages,
+    )
+    return message.content[0].text
+
+# putting into practice 
+
+messages = []
+
+add_user_message(messages, "Define quantum computing in one sentence")
+
+answer = chat(messages)
+
+add_assistant_message(messages, answer)
+
+add_user_message(messages, "Write Another Sentence")
+
+final_answer = chat(messages)
+
+print(answer)
+
+print(final_answer)
+
+
