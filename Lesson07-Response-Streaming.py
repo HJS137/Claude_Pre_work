9b5b3354580e86ebbebbb8ff8950c028a1dbaf82
HJS137 <harry.stevens@talktalk.net>
2026-09-15 18:54:35 +0100

    Lesson 7 on API training

---
+++ Lesson07-Response-Streaming.py
@@ -0,0 +1,51 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from anthropic import Anthropic
+
+client = Anthropic()
+model = "claude-haiku-4-5-20251001"
+
+
+def add_user_message(messages, text):
+    messages.append({"role": "user", "content": text})
+
+messages = []
+add_user_message(messages, "Write a 1 sentence description of a fake database")
+stream = client.messages.create(
+    model=model,
+    max_tokens=1000,
+    messages=messages,
+    stream=True
+)
+
+
+for event in stream:
+    print(event)
+
+
+
+##Making this more simplified:
+
+with client.messages.stream(
+    model=model,
+    max_tokens=1000,
+    messages=messages
+) as stream:
+    for text in stream.text_stream:
+        print(text, end="")
+
+
+### Getting the complete Message 
+
+with client.messages.stream(
+    model=model,
+    max_tokens=1000,
+    messages=messages
+) as stream:
+    for text in stream.text_stream:
+        pass
+
+    final_message = stream.get_final_message()
+
+
